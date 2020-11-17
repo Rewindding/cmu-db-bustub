7101e114c65e97179722a4d7f1b12437c15854d8
rewindding <rewindding@outlook.com>
2020-11-17 16:18:05 +0800

    index done

--- src/storage/index/index_iterator.cpp
+++ src/storage/index/index_iterator.cpp
@@ -12,20 +12,50 @@ namespace bustub {
  * set your own input parameters
  */
 INDEX_TEMPLATE_ARGUMENTS
-INDEXITERATOR_TYPE::IndexIterator() = default;
+INDEXITERATOR_TYPE::IndexIterator(page_id_t pageId, int Index, BufferPoolManager* bmp) :bufferPoolManager(bmp),leafPageId(pageId),kvIndex(Index) {}
 
 INDEX_TEMPLATE_ARGUMENTS
 INDEXITERATOR_TYPE::~IndexIterator() = default;
 
 INDEX_TEMPLATE_ARGUMENTS
-bool INDEXITERATOR_TYPE::isEnd() { throw std::runtime_error("unimplemented"); }
+bool INDEXITERATOR_TYPE::isEnd() {
+    Page* page = bufferPoolManager->FetchPage(leafPageId);
+    B_PLUS_TREE_LEAF_PAGE_TYPE* leafPage = reinterpret_cast<B_PLUS_TREE_LEAF_PAGE_TYPE* >(page->GetData());
+    if (leafPage->GetNextPageId() != INVALID_PAGE_ID) {
+       return false; 
+    }
+    bufferPoolManager->UnpinPage(page->GetPageId(),false);
+    return kvIndex == leafPage->GetSize() - 1;
+}
 
 INDEX_TEMPLATE_ARGUMENTS
-const MappingType &INDEXITERATOR_TYPE::operator*() { throw std::runtime_error("unimplemented"); }
+const MappingType &INDEXITERATOR_TYPE::operator*() { 
+    Page* page = bufferPoolManager->FetchPage(leafPageId);
+    B_PLUS_TREE_LEAF_PAGE_TYPE* leafPage = reinterpret_cast<B_PLUS_TREE_LEAF_PAGE_TYPE* >(page->GetData());
+    bufferPoolManager->UnpinPage(page->GetPageId(),false);
+    return leafPage->GetItem(kvIndex);
+}
 
 INDEX_TEMPLATE_ARGUMENTS
-INDEXITERATOR_TYPE &INDEXITERATOR_TYPE::operator++() { throw std::runtime_error("unimplemented"); }
-
+INDEXITERATOR_TYPE &INDEXITERATOR_TYPE::operator++() { 
+    Page* page = bufferPoolManager->FetchPage(leafPageId);
+    B_PLUS_TREE_LEAF_PAGE_TYPE* leafPage = reinterpret_cast<B_PLUS_TREE_LEAF_PAGE_TYPE* >(page->GetData());
+    ++kvIndex;
+    if(kvIndex >= leafPage->GetSize()) {
+        kvIndex = 0;
+        leafPageId = leafPage->GetNextPageId();
+    }
+    bufferPoolManager->UnpinPage(page->GetPageId(),false);
+    return *this; // ???
+}
+INDEX_TEMPLATE_ARGUMENTS
+bool INDEXITERATOR_TYPE::operator==(const IndexIterator &itr) const {
+    return itr.leafPageId==this->leafPageId&&itr.kvIndex==this->kvIndex;
+}
+INDEX_TEMPLATE_ARGUMENTS
+bool INDEXITERATOR_TYPE::operator!=(const IndexIterator &itr) const {
+    return itr.leafPageId!=this->leafPageId||itr.kvIndex!=this->kvIndex;
+}
 template class IndexIterator<GenericKey<4>, RID, GenericComparator<4>>;
 
 template class IndexIterator<GenericKey<8>, RID, GenericComparator<8>>;
