97a4168818880fedd788e16afc44dd5e1b092490
shilinlong <shilinlong@bytedance.com>
2021-06-09 23:50:40 +0800

    query executor first commit

--- src/execution/index_scan_executor.cpp
+++ src/execution/index_scan_executor.cpp
@@ -13,10 +13,38 @@
 
 namespace bustub {
 IndexScanExecutor::IndexScanExecutor(ExecutorContext *exec_ctx, const IndexScanPlanNode *plan)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), index_iterator_(nullptr) {}
 
-void IndexScanExecutor::Init() {}
+void IndexScanExecutor::Init() { index_iterator_.reset(nullptr); }
 
-bool IndexScanExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+bool IndexScanExecutor::Next(Tuple *tuple, RID *rid) {
+  index_oid_t indexOid = plan_->GetIndexOid();
+  IndexInfo *indexInfo = exec_ctx_->GetCatalog()->GetIndex(indexOid);
+  // how to retrive every rid from index??
+  // if the index type is not sure? how to handle then?
+  BPlusTreeIndex<GenericKey<8>, RID, GenericComparator<8>> *index =
+      static_cast<BPlusTreeIndex<GenericKey<8>, RID, GenericComparator<8>> *>(indexInfo->index_.get());
+  if (index_iterator_ == nullptr) {
+    index_iterator_ =
+        std::make_unique<IndexIterator<GenericKey<8>, RID, GenericComparator<8>>>(index->GetBeginIterator());
+  }
+  IndexIterator<GenericKey<8>, RID, GenericComparator<8>> endIterator = index->GetEndIterator();
+  TableMetadata *tableMetadata = exec_ctx_->GetCatalog()->GetTable(indexInfo->table_name_);
+  while (index_iterator_.operator*() != endIterator) {
+    *rid = index_iterator_.operator*().operator*().second;
+    index_iterator_.operator*().operator++();
+    rid->GetPageId();
+    bool ok;
+    bool predicated = false;
+    ok = tableMetadata->table_->GetTuple(*rid, tuple, exec_ctx_->GetTransaction());
+    if (ok) {
+      predicated = plan_->GetPredicate()->Evaluate(tuple, &tableMetadata->schema_).GetAs<bool>();
+    }
+    if (predicated) {
+      return true;
+    }
+  }
+  return false;
+}
 
 }  // namespace bustub
--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -13,10 +13,28 @@
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan)
+    : AbstractExecutor(exec_ctx), plan_(plan) {}
 
-void SeqScanExecutor::Init() {}
+void SeqScanExecutor::Init() { iterator.reset(nullptr); }
 
-bool SeqScanExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+bool SeqScanExecutor::Next(Tuple *tuple, RID *rid) {
+  table_oid_t table_id = plan_->GetTableOid();
+  TableMetadata *tableMetadata = exec_ctx_->GetCatalog()->GetTable(table_id);
+  if (iterator == nullptr) {
+    iterator = std::make_unique<TableIterator>(tableMetadata->table_->Begin(exec_ctx_->GetTransaction()));
+  }
+  TableIterator end = tableMetadata->table_->End();
+  auto predicate = plan_->GetPredicate();
+  while (iterator->operator!=(end)) {
+    *tuple = iterator.operator*().operator*();
+    *rid = iterator.operator*()->GetRid();
+    (*iterator).operator++();
+    if (predicate == nullptr || predicate->Evaluate(tuple, &tableMetadata->schema_).GetAs<bool>()) {
+      return true;
+    }
+  }
+  return !iterator->operator==(end);
+}
 
 }  // namespace bustub
